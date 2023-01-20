27c9fecde60958bcd3d3270f441b3cc2fcdbdb0b
t-r-i-y-a <triyaroy0401@gmail.com>
2023-01-20 16:15:00 +0530

    Pages linked

---
+++ index.js
@@ -0,0 +1,30 @@
+
+$(".btn-beaches").click(function()
+{
+    window.location.href = "content/beaches_index.html";
+});
+
+$(".btn-mountains").click(function()
+{
+    window.location.href = "content/mountains_index.html";
+});
+
+$(".btn-temples").click(function()
+{
+    window.location.href = "content/temples_index.html";
+});
+
+$(".btn-clubs").click(function()
+{
+    window.location.href = "content/clubs_index.html";
+});
+
+$(".btn-restaurants").click(function()
+{
+    window.location.href = "content/restaurants_index.html";
+});
+
+$(".btn-road-trips").click(function()
+{
+    window.location.href = "content/roadtrips_index.html";
+});
